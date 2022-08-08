69393f6aca8f0d42667e2fb70b277e924ccee4cb
qvmb <110808146+qvmb@users.noreply.github.com>
2022-08-08 08:47:11 +0200

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
         
         return (
                 <>
-                  <p className={style.contact}>pgp: <a href="pgp.txt">vaul@vaul.xyz</a> <br></br>
+                  <p className={style.contact}>pgp: <a href="pgp.txt">wojtekdziarlowski@gmail.com</a> <br></br>
                   fingerprint: <code>D043 08DD EEB6 80ED 8DDF 82F4 3FEE FF70 8CDD A5F8</code>
                   </p>
                 </>
